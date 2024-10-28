5b7b9bcc9e5eef59f93d729aac359e7c888b43a8
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-10-28 18:12:36 +0800

    Update serv00_hy2.sh

--- serv00_hy2.sh
+++ serv00_hy2.sh
@@ -18,8 +18,14 @@ prompt_for_input() {
     echo "${input:-$default_value}"
 }
 
-IP=$(prompt_for_input "请输入 IP地址: " "{{IP}}")
-PORT1=$(prompt_for_input "输入 hysteria2的udp端口号: " "33333")
+IP_1=$(prompt_for_input "请输入第一个IP地址: " "{{IP_1}}")
+PORT_1=$(prompt_for_input "输入第一个udp端口号: " "33333")
+
+IP_2=$(prompt_for_input "请输入第一个IP地址: " "{{IP_2}}")
+PORT_2=$(prompt_for_input "输入第一个udp端口号: " "44444")
+
+IP_3=$(prompt_for_input "请输入第一个IP地址: " "{{IP_3}}")
+PORT_3=$(prompt_for_input "输入第一个udp端口号: " "55555")
 
 # 配置文件备份路径
 BACKUP_CONFIG_FILE="config_backup.json"
@@ -37,7 +43,9 @@ if [ -f "$BACKUP_CONFIG_FILE" ]; then
     cp "$BACKUP_CONFIG_FILE" "$NEW_CONFIG_FILE"  # 从备份文件复制新文件
 
     echo "替换配置文件中的占位符..."
-    sed -i '' "s/{{IP}}/$IP/g; s/33333/$PORT1/g" "$NEW_CONFIG_FILE"  # 替换新配置文件中的占位符
+    sed -i '' "s/{{IP_1}}/$IP_1/g; s/33333/$PORT_1/g" "$NEW_CONFIG_FILE"  # 替换新配置文件中的占位符
+    sed -i '' "s/{{IP_2}}/$IP_2/g; s/44444/$PORT_2/g" "$NEW_CONFIG_FILE"  # 替换新配置文件中的占位符
+    sed -i '' "s/{{IP_3}}/$IP_3/g; s/55555/$PORT_3/g" "$NEW_CONFIG_FILE"  # 替换新配置文件中的占位符
 else
     echo "配置文件 $BACKUP_CONFIG_FILE 不存在！"
     exit 1
@@ -51,5 +59,5 @@ echo "启动服务..."
 screen -dmS box /home/${USERNAME}/sing-box/sb run
 echo "启动服务: successful\n服务已成功启动。要重新附加到屏幕会话，请使用：screen -r box\n请测试下面的订阅:"
 echo ""
-echo -e "\033[0;32m hysteria2://$UUID@$IP:$PORT1/?sni=www.bing.com&alpn=h3&insecure=1#${USERNAME} \033[0m"
+echo -e "\033[0;32m hysteria2://$UUID@$IP_1:$PORT_1/?sni=www.bing.com&alpn=h3&insecure=1#${USERNAME} \033[0m"
 echo ""
