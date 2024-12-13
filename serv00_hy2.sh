f4aa6edb2feef14b1771f934753ded1d931968a8
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-12-13 14:27:49 +0800

    Update serv00_hy2.sh

--- serv00_hy2.sh
+++ serv00_hy2.sh
@@ -17,6 +17,7 @@ prompt_for_input() {
     read -p "$(echo -e '\033[0;32m'"$prompt_msg"' \033[0m')" input
     echo "${input:-$default_value}"
 }
+UUID=$(prompt_for_input "输入UUID:" "ccc33d85-681b-41f7-b9db-079ed095d2df")
 PORT1=$(prompt_for_input "输入配置的第一个udp端口号: " "33333")
 IP_1=$(prompt_for_input "请输入第 1 个IP地址: " "{{IP_1}}")
 PORT2=$(prompt_for_input "输入配置的第二个udp端口号: " "44444")
@@ -29,17 +30,18 @@ NEW_CONFIG_FILE="config.json"
 # 删除旧的 config.json 文件
 if [ -f "$NEW_CONFIG_FILE" ]; then
     echo "删除旧的 $NEW_CONFIG_FILE 文件..."
-    rm -f "$NEW_CONFIG_FILE"  # 删除旧的 config.json 文件
+    rm -f "$NEW_CONFIG_FILE"
 fi
 
 # 检查备份配置文件是否存在
 if [ -f "$BACKUP_CONFIG_FILE" ]; then
     echo "复制 $BACKUP_CONFIG_FILE 为 $NEW_CONFIG_FILE..."
-    cp "$BACKUP_CONFIG_FILE" "$NEW_CONFIG_FILE"  # 从备份文件复制新文件
+    cp "$BACKUP_CONFIG_FILE" "$NEW_CONFIG_FILE"
 
     echo "替换配置文件中的占位符..."
-    sed -i '' "s/{{IP_1}}/$IP_1/g; s/33333/$PORT1/g" "$NEW_CONFIG_FILE"  # 替换新配置文件中的占位符
-    sed -i '' "s/{{IP_2}}/$IP_2/g; s/44444/$PORT2/g" "$NEW_CONFIG_FILE"  # 替换新配置文件中的占位符
+    sed -i '' "s/a03e977f-6491-42a2-b56d-abbab6c3a9ac/$UUID/g" "$NEW_CONFIG_FILE"
+    sed -i '' "s/{{IP_1}}/$IP_1/g; s/33333/$PORT1/g" "$NEW_CONFIG_FILE"
+    sed -i '' "s/{{IP_2}}/$IP_2/g; s/44444/$PORT2/g" "$NEW_CONFIG_FILE"
 else
     echo "配置文件 $BACKUP_CONFIG_FILE 不存在！"
     exit 1
