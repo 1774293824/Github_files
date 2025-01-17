8382cf6be08a9795e1ee2378fc79c562f70282ba
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-01-17 10:30:51 +0800

    Update serv00_vless.sh

--- serv00_vless.sh
+++ serv00_vless.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -e
+
+WORK_DIR="./vless"
+mkdir -p "$WORK_DIR" && cd "$WORK_DIR"
 
 # 下载并解压缩文件
 wget -q 'https://github.com/1774293824/Github_files/releases/download/serv00/vless.zip'
@@ -6,4 +10,4 @@ echo "下载完成"
 unzip vless.zip
 
 echo "安装完成"
-请运行 【 screen -dmS vless node /home/$(whoami)/vless/app.js 】
+echo "请运行 【 screen -dmS vless node /home/$(whoami)/vless/app.js 】"
