a2e93b1be7bf5d0f19cac6cc949e9dc9230d3f02
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-05-14 10:31:46 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -26,7 +26,7 @@ prompt_for_input() {
 }
 UUID=$(prompt_for_input "输入UUID:" "ccc33d85-681b-41f7-b9db-079ed095d2df")
 PORT1=$(prompt_for_input "输入配置的第一个udp端口号: " "33333")
-IP_1=$(prompt_for_input "请输入第 1 个IP地址: " "{{IP_1}}")
+IP_1=$(prompt_for_input "请输入第 1 个IP地址(回车默认配置0.0.0.0): " "0.0.0.0")
 PORT2=$(prompt_for_input "输入配置的第二个udp端口号: " "44444")
 IP_2=$(prompt_for_input "请输入第 2 个IP地址: " "{{IP_2}}")
 
